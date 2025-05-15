3e9b6ab2fc7fc2a3a8757ffcf6697b630051e917
Ivan Alcantar <trashbandit9811@gmail.com>
2025-05-14 22:04:03 -0600

    feat(4.3/example1.py).:ADD 1 EXAMPLE FILE

---
+++ Module4/4.3 Return/example1.py
@@ -0,0 +1,12 @@
+def happy_new_year(wishes = True):
+    print("Three...")
+    print("Two...")
+    print("One...")
+    if not wishes:
+        return
+
+    print("Happy New Year!")
+
+# Test the function
+
+happy_new_year()
