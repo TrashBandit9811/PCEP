5f360d8c60ef05e75bd645d7327d35f20a27a7c3
Ivan Alcantar <trashbandit9811@gmail.com>
2025-05-18 21:16:03 -0600

    feat(4.5): Complete Module 4.5 Multi-parameter functions

---
+++ Module4/4.5 Multi-parameter functions/example3.py
@@ -0,0 +1,12 @@
+def lb_to_kg(lb):
+    return lb * 0.45359237
+
+
+print(lb_to_kg(1))
+
+def ft_and_inch_to_m(ft, inch = 0.0):
+    return ft * 0.3048 + inch * 0.0254
+
+
+print(ft_and_inch_to_m(6))
+
