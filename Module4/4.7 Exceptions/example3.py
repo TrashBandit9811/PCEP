b9b20e2e0142a44cac5614269d1e08f432fe622a
Ivan Alcantar <trashbandit9811@gmail.com>
2025-06-14 22:33:03 -0600

    feat(4.7/ex3): added comments on program feature

--- Module4/4.7 Exceptions/example3.py
+++ Module4/4.7 Exceptions/example3.py
@@ -3,4 +3,5 @@ try:
     print('The reciprocal of', value, 'is', 1/value)        
 except:
     print('I do not know what to do.')
-# The code above is a simple example of exception handling in Python. 
+# The code above is a simple example of exception handling in Python.
+# It attempts to convert user input into an integer and calculate its reciprocal.
